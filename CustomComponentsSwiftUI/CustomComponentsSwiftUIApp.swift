6e583f2e1687e515001eba48b358d89ace1b8489
Vika Smet <vika2001-04@mail.ru>
2024-12-02 20:51:39 +0300

    git init

---
+++ CustomComponentsSwiftUI/CustomComponentsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomComponentsSwiftUIApp.swift
+//  CustomComponentsSwiftUI
+//
+//  Created by Vika on 02.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomComponentsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
