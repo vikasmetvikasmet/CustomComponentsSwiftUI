a2a7bb23cc6e5eb3039f845617ffc1bd7d99e322
Vika Smet <vika2001-04@mail.ru>
2024-12-02 21:18:36 +0300

    task#1

--- CustomComponentsSwiftUI/ContentView.swift
+++ CustomComponentsSwiftUI/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    var numbers: [Int] = Array(1...100)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Swift!")
+        TabView {
+            ScrollView {
+                LazyVStack {
+                    ForEach(numbers, id: \.self) { number in
+                        NumberView(number: number)
+                    }
+                }
+            }.safeAreaInset(edge: .bottom, spacing: 0) {
+                BottomBar()
+            }
+            .tabItem {
+                Label("First", systemImage: "star.fill")
+            }
         }
-        .padding()
+    }
+}
+
+struct NumberView: View {
+    let number: Int
+    var body: some View {
+        Text("\(number)")
+            .foregroundColor(.gray)
+            .font(.system(size: 30))
+            .opacity(0.5)
+    }
+}
+struct BottomBar: View {
+    var body: some View {
+        Rectangle()
+            .frame(maxWidth: .infinity)
+            .frame(height: 50)
+            .foregroundStyle(Color.red)
+            .opacity(0.5)
     }
 }
 
