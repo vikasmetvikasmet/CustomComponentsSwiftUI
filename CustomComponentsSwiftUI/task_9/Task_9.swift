cccd2c07c15455f967bf20354b12a4fbd1106846
Vika Smet <vika2001-04@mail.ru>
2024-12-31 18:28:18 +0300

    tak#9.1

--- CustomComponentsSwiftUI/task_9/Task_9.swift
+++ CustomComponentsSwiftUI/task_9/Task_9.swift
@@ -8,76 +8,65 @@
 import SwiftUI
 
 struct Task_9: View {
-    @State private var isDragging = false
+    
     @State var dragOffset = CGSize.zero
     
     var body: some View {
-        
         ZStack {
-            Color.red
-            Color.yellow
-                .clipShape(Circle())
-                .frame(width: 200, height: 200)
-                .blur(radius: 15)
+            Color.black.ignoresSafeArea()
             
+            RadialGradient(colors: [.yellow, .red], center: .center, startRadius: 70, endRadius: 160)
+                .mask {
+                    Canvas { context, size in
+                        context.addFilter(.alphaThreshold(min: 0.5, color: .white))
+                        context.addFilter(.blur(radius: 40))
+                        
+                        context.drawLayer { ctx in
+                            for index in [1, 2] {
+                                if let resolved = context.resolveSymbol(id: index) {
+                                    ctx.draw(resolved, at: .init(x: size.width / 2, y: size.height / 2))
+                                }
+                            }
+                        }
+                        
+                    } symbols: {
+                        ball
+                            .tag(1)
+                        ball
+                            .tag(2)
+                            .offset(dragOffset)
+                    }
+                    
+                }
             Image(systemName: "cloud.sun.rain.fill")
                 .symbolRenderingMode(.hierarchical)
                 .resizable()
-                .frame(width: 80, height: 80)
-                .foregroundColor(.white)
+                .scaledToFit()
+                .frame(width: 80)
                 .offset(dragOffset)
-            
-        }.mask {
-            Canvas { context, size in
-                let circleYellow = context.resolveSymbol(id: 1)
-                let circleRed = context.resolveSymbol(id: 2)
-                let rect = CGPoint(x: size.width / 2, y: size.height / 2)
+                .foregroundStyle(.white)
+
                 
-                context.addFilter(.alphaThreshold(min: 0.5))
-                context.addFilter(.blur(radius: 15))
-                
-                context.drawLayer { ctx in
-                    
-                    ctx.draw(circleYellow!, at: rect)
-                    ctx.draw(circleRed!, at: rect)
-                }
-                
-            } symbols: {
-                Circle()
-                    .frame(width: 150, height: 150)
-                    .offset(dragOffset)
-                    .tag(1)
-                Circle()
-                    .frame(width: 150, height: 150)
-                    .tag(2)
+        }
+        .gesture(DragGesture()
+            .onChanged{ drag in
+                dragOffset = drag.translation
             }
-        }.background(wallpaper)
-            .gesture(drag)
-            .ignoresSafeArea()
-    }
-    
-    var wallpaper: some View {
-        Image("capa")
-            .resizable()
-            .blur(radius: 7)
-            .scaleEffect(1.2)
-            .ignoresSafeArea()
-    }
-    
-    var drag: some Gesture {
-        DragGesture()
-            .onChanged { value  in
-                self.isDragging = true
-                dragOffset = value.translation
-            }
-            .onEnded { _ in
-                withAnimation(.interpolatingSpring(stiffness: 150, damping: 15)) {
-                    self.isDragging = false
+            .onEnded({ _ in
+                withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7),{
                     dragOffset = .zero
-                }
-                
+                })
             }
+                    )
+        )
     }
+    var ball: some View {
+        Circle()
+            .foregroundColor(.white)
+            .frame(width: 150, height: 150)
+    }
+    
+    
 }
 
 #Preview {
